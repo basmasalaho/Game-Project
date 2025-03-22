105ee23009d8e4a173b8f2b8ac8b3b31c9f5fe3f
basmasalaho <basmaslaho@gamail.com>
2025-03-22 10:44:26 +0300

    Fix duplicate Sidebar import and component name

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainContent from "./components/layout/MainContent";
 import NavBar from "./components/layout/NavBar";
-
-import Sidebar from './components/layout/Sidebar.jsx'
-
-import Sidebar from './components/layout/Sidebar/index.jsx'
+import Sidebar from './components/layout/Sidebar';
 import GameDetailPage from './components/layout/GameDetailPage';
 import { GameProvider } from './context/GameContext';
 
@@ -16,7 +13,7 @@ function App() {
       <GameProvider>
         <NavBar />
         <div>
-          <SideBar />
+          <Sidebar />
           <Routes>
             <Route path="/" element={<MainContent />} />
             <Route path="/game/:id" element={<GameDetailPage />} />
